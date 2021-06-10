13cd2c58e3cfe3b869ebe10efa4ac86a9dea93a5
Yuniati-Rafiah <rafiahyuniati@gmail.com>
2021-06-10 10:32:02 +0800

    Membuat Class, Atribut, dan Objek

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import model.Mahasiswa;
 
 public class Main {
     public static void main(String[] args) {
-        Mahasiswa m1 = new Mahasiswa("19631017","Regina","29-09-2000");
+        Mahasiswa m1 = new Mahasiswa("19630460","Yuniati","02-02-2000");
         m1.tampilkanAtribut();
         System.out.println(m1.getNama());
 
